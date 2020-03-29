ea4eef9b4b5377d9e3646778478df881fcf6e973
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-03-29 20:00:55 +0530

    RT for #DevOps

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import tweepy
+import time
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler("gmnr7Ko7CHjDpN3U7XG7O7B8E", "5E63V14UhWNPHgDcj4KysQ91R1BbMjpzaDaDgiJJo27ePinC8g")
@@ -15,4 +16,11 @@ except:
     print("Authentication Failure")
 
 # Create a Test tweet
-api.update_status("Test Tweet for Bot")
+#api.update_status("Test Tweet for Bot")
+
+#Retweet for #DevOps 
+for tweet in tweepy.Cursor(api.search,q='#DevOps').items(3):
+    print("\n RT by Bot")
+    tweet.retweet()
+    print("RT Success!")
+    time.sleep(5) #sleep for 5 seconds before next RT
