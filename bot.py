450d94512ad8d83b4415deede89080276fe6b76d
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-03-29 19:45:41 +0530

    Testing Tweet for Bot

---
+++ bot.py
@@ -0,0 +1,11 @@
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("gmnr7Ko7CHjDpN3U7XG7O7B8E", "5E63V14UhWNPHgDcj4KysQ91R1BbMjpzaDaDgiJJo27ePinC8g")
+auth.set_access_token("2427185198-0liiBHYMUc9vwmjYwkckw2ZNy7Gu2yzGId0XcQO", "Ri9XxroE4foXfm9c64oaPkFr0frwX4HxWJTXIGPGCHCEY")
+
+# Create API object
+api = tweepy.API(auth)
+
+# Create a Test tweet
+api.update_status("Test Tweet for Bot")
